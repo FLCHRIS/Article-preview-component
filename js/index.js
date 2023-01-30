c80deb03cc85ae66d9617ca05e84c4a8743c1f39
Christian <filetelopezchristian@gmail.com>
2023-01-30 15:28:13 -0600

    Se modificó todo el proyecto

--- js/index.js
+++ js/index.js
@@ -1,13 +1,6 @@
-const button = document.querySelector('.social-button');
+const showButton = document.querySelector('.card__button');
+const showDiv = document.querySelector('.card__socialMedia');
 
-button.addEventListener('click', () => {
-    if(screen.availWidth >= 750){
-        document.querySelector('.social__ctas').classList.toggle('social__ctas-show');
-        console.log('aaa')
-    }else {
-        console.log('bbbb')
-        document.querySelector('.social__ctas').classList.toggle('social__ctas-show');
-        document.querySelector('.social__person').classList.toggle('social__person-hidden');
-        document.querySelector('.card__social').classList.toggle('card__information-active');
-    }
+showButton.addEventListener('click', () => {
+    showDiv.classList.toggle('card__socialMedia-hidden');
 })
